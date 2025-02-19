4defb78cf028baf0e643d43ebd9d113b4ab00d81
kalimullah41 <kalimullah41557@gmail.com>
2025-02-19 11:59:28 +0530

    initial commit

---
+++ src/main/java/in/kalim/Welcome.java
@@ -0,0 +1,21 @@
+package in.kalim;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Kalimullah IT...!!!");
+		
+		 // Task -101 changes 
+		int i =10;
+		int j =20;
+		 int k = i+j;
+		 
+		 System.out.println(k);
+		 
+				
+				
+		
+	}
+	
+	
+}
